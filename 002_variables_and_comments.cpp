82172e2408a069b64993aacd34e8c457c52ae190
Student9876 <shouvik9876@gmail.com>
2022-07-23 22:27:35 +0530

    Start of C++

---
+++ 002_variables_and_comments.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+//This program was created by zeon. This is a single line comment
+/*This is 
+a multi line comment */
+int main()
+{
+    int sum = 6;
+    cout<< "Variable"<<sum;
+    return 0;
+}
