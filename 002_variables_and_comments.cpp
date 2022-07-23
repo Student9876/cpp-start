ea03622bc9f12edd6b16767a725d3d8a99d8c5db
Student9876 <shouvik9876@gmail.com>
2022-07-24 01:00:43 +0530

    gitignore changes

--- 002_variables_and_comments.cpp
+++ 002_variables_and_comments.cpp
@@ -7,6 +7,34 @@ a multi line comment */
 int main()
 {
     int sum = 6;
-    cout<< "Variable"<<sum;
+    cout<< "Variable"<< sum;
     return 0;
 }
+
+/*
+Variables in C++
+
+1. int --> 1,2,-1,67 etc.
+2. Float--> 1.21,4.56 etc.
+3. Char --> 'a', 'r'
+4. Double--> Used for storing large data like- 1.214653...
+5. Boolean--> Male or Female
+                Yes or No
+
+## Based on Scope, variables can be classified into two types:
+->  Scope of a variable is the region in code wherethe
+    existance of variable is valid
+
+1. Local Variable--> Local variables are declared inside the braces
+    of any function and be accessed only from there.
+
+2. Global Variable--> Global variables are declared outside any function and 
+    can be accessed from anywhere.
+
+*Global and local variables can have same name.
+
+## C++ Data types
+1. Built in 
+2. User Defined
+3. Derived
+*/
