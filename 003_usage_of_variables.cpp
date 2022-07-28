c6fcea4fa77901c0c9f5c5afbed0519718482fc9
Student9876 <shouvik9876@gmail.com>
2022-07-28 09:39:03 +0530

    Variables

--- 003_usage_of_variables.cpp
+++ 003_usage_of_variables.cpp
@@ -20,6 +20,7 @@ int main()
     float pi;
     char c = 'u'; //only one character can be used. We cannot use it like c ='ghgt'
     bool is_true=false;
+    pi = 3.14;
     cout<<glo<< is_true; //prints 71, 7 glo value and 1 for true. prints 70, 0 for false
     cout<<"\nThe value of a is "<<a<<". \nThe value of b is "<<b;
     cout<<"\nThe value of pi is "<<pi;
