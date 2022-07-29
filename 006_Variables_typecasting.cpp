7e390204ccb0f3b625a0a2ce84e8037370b145de
Student9876 <shouvik9876@gmail.com>
2022-07-29 11:02:30 +0530

    Variables and typecasting

--- 006_Variables_typecasting.cpp
+++ 006_Variables_typecasting.cpp
@@ -9,37 +9,37 @@ int main()
 {
 //***************Built in data types**************
 
-    // int a, b, c;
-    // cout<<"Enter value of a and b: "<<endl;
-    // cin>>a>>b;
+    int a, b, c;
+    cout<<"Enter value of a and b: "<<endl;
+    cin>>a>>b;
 
-    // c = a+b;
-    // cout<<"The sum is:"<<c<<endl;
-    // cout<<"The global c is:"<<::c<<endl; //'::' is scope resolution operator, Used to get global var
-    // cout<<endl;
+    c = a+b;
+    cout<<"The sum is:"<<c<<endl;
+    cout<<"The global c is:"<<::c<<endl; //'::' is scope resolution operator, Used to get global var
+    cout<<endl;
 
 //***************float, double and long double Literals**************
 
     //by default 35.6 is a Double in C++. So to pass the number as a float type 35.6f
-    // float d = 35.6f; //f for making the number float
-    // long double e = 35.6l; //l for making the number long double
-    // cout<<"The value of float is:"<<d<<endl<<"The value of long double is: "<<e<<endl;
+    float d = 35.6f; //f for making the number float
+    long double e = 35.6l; //l for making the number long double
+    cout<<"The value of float is:"<<d<<endl<<"The value of long double is: "<<e<<endl;
 
 
 
-    // cout<<"The size of 34.4 is:"<<sizeof(34.4)<<endl;
-    // cout<<"The size of 34.4f is:"<<sizeof(34.4f)<<endl;
-    // cout<<"The size of 34.4F is:"<<sizeof(34.4F)<<endl;
-    // cout<<"The size of 34.4l is:"<<sizeof(34.4l)<<endl;
-    // cout<<"The size of 34.4L is:"<<sizeof(34.4L)<<endl;
+    cout<<"The size of 34.4 is:"<<sizeof(34.4)<<endl;
+    cout<<"The size of 34.4f is:"<<sizeof(34.4f)<<endl;
+    cout<<"The size of 34.4F is:"<<sizeof(34.4F)<<endl;
+    cout<<"The size of 34.4l is:"<<sizeof(34.4l)<<endl;
+    cout<<"The size of 34.4L is:"<<sizeof(34.4L)<<endl;
 
 //***************Reference Variables**************
 // Rohan --> Monty --> Rohu --> Dangerous Coder
-    // float x = 455;
-    // float & y =x;
+    float x = 455;
+    float & y =x;
 
-    // cout<<x<<endl;
-    // cout<<y<<endl;
+    cout<<x<<endl;
+    cout<<y<<endl;
 
 
 //***************Typecasting**************
