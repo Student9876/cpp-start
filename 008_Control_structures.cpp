bf49020a23660fe3f995e7f59abfa90e263b4b70
Student9876 <shouvik9876@gmail.com>
2022-07-30 21:03:24 +0530

    Control Structures

---
+++ 008_Control_structures.cpp
@@ -0,0 +1,21 @@
+/* C++ control structures
+1.Sequence Structures
+2.Selection Structures
+3.Loop Structures
+*/
+
+/* Sequence Structure
+Suppose: Entry-->Action 1-->Action 2--> Exit
+*/
+
+/* Selection Structure....
+Entry---> If True-->Action 1
+        If False-->Action 2
+        Exit
+*/
+
+/* Loop Structure....
+
+Entry-->(Loop) -->  Condition ---> If True--> Action 1-->(Loop)
+                                    If False--> Action 2--> Exit
+*/                                    
