1fb7f8c53a43a8dd81a3c6f5153abd886b75e783
Student9876 <shouvik9876@gmail.com>
2022-08-01 22:04:28 +0530

    Static Integer

---
+++ 018_static_int.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int product(int a, int b)
+{
+    static int c=0; //this executes only once
+                    //static int variables initializes only one time.
+    c= c+1;         // Here the value of c increases after each time the function executes.
+    return a*b+c;
+}
+
+int main()
+
+{
+    int a,b;
+    cout<<"Enter the valur e of a & b: "<<endl;
+    cin>>a>>b;
+    cout<<"Product of a and b is: "<<product(a,b)<<endl;
+    cout<<"Product of a and b is: "<<product(a,b)<<endl;
+    cout<<"Product of a and b is: "<<product(a,b)<<endl;
+    cout<<"Product of a and b is: "<<product(a,b)<<endl;
+    cout<<"Product of a and b is: "<<product(a,b)<<endl;
+    return 0;
+}
