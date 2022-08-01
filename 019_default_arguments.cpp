12d54ef0c302a1e3227c2e372630cb21ce1b12b2
Student9876 <shouvik9876@gmail.com>
2022-08-01 22:04:50 +0530

    Default Arguments

---
+++ 019_default_arguments.cpp
@@ -0,0 +1,19 @@
+//default arguments
+#include<iostream>
+using namespace std;
+
+float moneyReceived(int currentMoney, float factor = 1.04) //factor has a default value
+{
+    return currentMoney*factor;
+}
+
+int main()
+
+{
+    // int a,b;
+    // cout<<"Enter the value of a and b: "<<endl;
+    int money = 100000;
+    cout<<"If you have "<<money<<" Rs in your bank account, you will receive "<<moneyReceived(money)<<" Rs after 1 year"<<endl;
+    cout<<"For VIP: If you have "<<money<<" Rs in your bank account, you will receive "<<moneyReceived(money, 1.10)<<" Rs after 1 year"<<endl;
+    return 0;
+}
