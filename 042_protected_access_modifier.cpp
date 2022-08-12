2c3bf766a74dc793a274c19f9a46c99f8047d009
Student9876 <shouvik9876@gmail.com>
2022-08-12 13:40:41 +0530

    Protected access modifier

---
+++ 042_protected_access_modifier.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+    protected:
+        int a;
+    private:
+        int b;
+
+};
+
+/*
+For a protected member:
+                        Public Derivation       Private Derivation      Protected Derivation
+1. Private Member       Not inherited           Not inherited               Not Inherited
+1. Protected Member        Protected            private                     protected
+1. Public Member            Public              Private                     Protected
+*/
+class Derived : protected Base{
+
+};
+int main()
+{
+    Base b;
+    Derived d;
+    // cout<<d.a; //will not work since a is protected in both base as well as derived class
+    return 0;
+}
