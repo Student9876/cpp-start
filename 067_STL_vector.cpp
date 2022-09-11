aff8f97f6301c7a409eedd3ea6230d8b4e74931f
student9876 <shouvik9876@gmail.com>
2022-09-11 13:21:43 +0530

    Edited STL vector

--- 067_STL_vector.cpp
+++ 067_STL_vector.cpp
@@ -18,33 +18,33 @@ int main()
 {
     // ways to create a vector
     vector<int> vec1; //zero length vector 
-    vector<char> vec2(4); //4-element character vector
-    vec2.push_back('5');
-    display(vec2);
+    // vector<char> vec2(4); //4-element character vector
+    // vec2.push_back('5');
+    // display(vec2);
 
-    vector<char> vec3(vec2); //4-element character vector from vec2
-    display(vec3);
+    // vector<char> vec3(vec2); //4-element character vector from vec2
+    // display(vec3);
 
-    vector<int> vec4(6,13); //6-element vector of 3s
-    display(vec4);
-    cout<<vec4.size()<<endl;
+    // vector<int> vec4(6,13); //6-element vector of 3s
+    // display(vec4);
+    // cout<<vec4.size()<<endl;
 
 
 
-// // First program
-    // int element,size ;
-    // cout<<"Enter the size of the vector: ";
-    // cin>>size;
-    // for (int i = 0; i < size; i++)
-    // {
-    //     cout << "Enter an element to add to this vector ";
-    //     cin >> element;
-    //     vec1.push_back(element);
-    // }
-//     display(vec1);
+// First program
+    int element,size ;
+    cout<<"Enter the size of the vector: ";
+    cin>>size;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter an element to add to this vector ";
+        cin >> element;
+        vec1.push_back(element);
+    }
+    display(vec1);
 
-//     vec1.pop_back();
-//     display(vec1);
+    vec1.pop_back();
+    display(vec1);
 
 // //deriving an iterator
 // vector<int> :: iterator iter = vec1.begin();
@@ -58,8 +58,8 @@ int main()
 //     display(vec1);
 
 
-//     //inserting multiple values 
-//     vec1.insert(iter+3,500, 69);
-//     display(vec1);
+    //inserting multiple values 
+    // vec1.insert(iter+3,500, 69);
+    // display(vec1);
     return 0;
 }
